b0fabe90f92cc3dc13c5eabbf62ad01f33c3a467
zhaoguorui666 <18235787078@163.com>
2019-12-16 22:37:51 -0800

    week13

---
+++ code/my.h
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <time.h>
+#include <wait.h>
